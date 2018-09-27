3f7abf03bdbbf685b37186ed6334456f7e62b180
Rik Meijer <ha.meijer@avans.nl>
2018-09-27 16:50:27 +0200

    add IS example: a driver only needs to know wether a car can be driven (DrivableCar). The engineer only needs a RefuelableCar.

---
+++ OO/src/InterfaceSegregation/Bad/PetrolCar.java
@@ -0,0 +1,16 @@
+package InterfaceSegregation.Bad;
+
+/**
+ * @author Rik Meijer ha.meijer@avans.nl
+ */
+public class PetrolCar {
+
+    public void accelerate() {
+        // accelerate statements
+    }
+
+    public void refuel() {
+        // refuel statements
+    }
+
+}
---
+++ OO/src/InterfaceSegregation/Good/Driver.java
@@ -0,0 +1,12 @@
+package InterfaceSegregation.Good;
+
+/**
+ * @author Rik Meijer ha.meijer@avans.nl
+ */
+public class Driver {
+
+    public void driveCar(DrivableCar car) {
+        car.accelerate();
+    }
+
+}
